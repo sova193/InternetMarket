5da89c1c9afffa289ab45aa3de969977e42987e7
sova193 <sova193@yandex.ru>
2023-05-25 11:23:03 +0300

    Добавил поля о времени заказа и фамилии сотрудников

--- sova_market/models.py
+++ sova_market/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 director = 'Di'
 admin = 'AD'
@@ -19,6 +20,9 @@ class Staff(models.Model):
     position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
     labor_contract = models.IntegerField()
 
+    def get_last_name(self):
+        return self.full_name.split()[0]
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -36,8 +40,23 @@ class Order(models.Model):
 
     products = models.ManyToManyField(Product, through='ProductOrder')
 
+    def finish_order(self):
+        self.time_out = datetime.now()
+        self.complete = True
+        self.save()
+
+    def get_duration(self):
+        if self.complete:  # если завершён, возвращаем разность объектов
+            return (self.time_out - self.time_in).total_seconds() // 60
+        else:  # если ещё нет, то сколько длится выполнение
+            return (datetime.now(timezone.utc) - self.time_in).total_seconds() // 60
+
 
 class ProductOrder(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     amount = models.IntegerField(default=1)
+
+    def product_sum(self):
+        product_price = self.product.price
+        return product_price * self.amount
