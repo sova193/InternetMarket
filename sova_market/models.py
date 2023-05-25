ec8927920f016a58be1e8d3803b21ba41619f7e9
sova193 <sova193@yandex.ru>
2023-05-25 16:09:25 +0300

    full vershon 16.09

--- sova_market/models.py
+++ sova_market/models.py
@@ -1,15 +1,27 @@
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.validators import UnicodeUsernameValidator
+
 from django.db import models
-from datetime import datetime, timezone
+from datetime import datetime
 
 from django.db.models import DateTimeField
-from sova_market.resources import POSITIONS, cashier
 
+director = 'Di'
+admin = 'AD'
+salesman = 'SM'
+cashier = 'CA'
+cleaner = 'CL'
+courier = 'CR'
 
+POSITIONS = [
+    (director, 'Директор'),
+    (admin, 'Администратор'),
+    (salesman, 'Упаковщик'),
+    (cashier, 'Кассир'),
+    (cleaner, 'Уборщик'),
+    (courier, 'Курьер')
+]
 class Staff(models.Model):
     full_name = models.CharField(max_length=255)
-    position = models.CharField(max_length=2, choices=POSITIONS, default=cashier)
+    position = models.CharField(max_length=255, choices=POSITIONS, default=cashier)
     labor_contract = models.IntegerField()
 
     def get_last_name(self):
@@ -62,47 +74,3 @@ class ProductOrder(models.Model):
         product_price = self.product.price
         return product_price * self.amount
 
-
-class AbstractBaseUser(models.Model):
-    password = models.CharField(_('password'), max_length=128)
-    last_login = models.DateTimeField(_('last login'), blank=True, null=True)
-
-    is_active = True
-
-
-class AbstractUser(AbstractBaseUser, PermissionsMixin):
-    """
-    An abstract base class implementing a fully featured User model with
-    admin-compliant permissions.
-
-    Username and password are required. Other fields are optional.
-    """
-    username_validator = UnicodeUsernameValidator()
-
-    username = models.CharField(
-        _('username'),
-        max_length=150,
-        unique=True,
-        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
-        validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
-    )
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
-    is_staff = models.BooleanField(
-        _('staff status'),
-        default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
-    )
-    is_active = models.BooleanField(
-        _('active'),
-        default=True,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
