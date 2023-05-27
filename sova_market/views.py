7848aef9ad89992ab60caba18b449b128382ffac
sova193 <sova193@yandex.ru>
2023-05-27 19:13:47 +0300

    обновил перед удалением БД из-за ошибок

--- sova_market/views.py
+++ sova_market/views.py
@@ -1,17 +1,22 @@
+from datetime import datetime
 from django.views.generic import ListView, DetailView
 from .models import Product
 
 class ProductsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Product
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = 'name'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'products.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        context['time_now'] = datetime.utcnow()
+
+        context['next_sale'] = "Распродажи не будет"
+        return context
 
 
 class ProductDetail(DetailView):
